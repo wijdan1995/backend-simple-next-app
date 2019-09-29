591042c77c65be7130add848d1063cbc8c63b683
wijdan1995 <wijdan.w.k@outlook.sa>
2019-09-29 19:32:53 +0300

    setup for deployment

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(cors())
+
+// Define Ports
+const reactPort = 3000
+const expressPort = 1234
+
+// set CORS headers on response from this API using the `cors` NPM package
+// `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
+// app.use(cors())
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${reactPort}` }))
+
 
 const mongoose = require('mongoose')
 // mongoDB connection
@@ -23,5 +32,5 @@ const contactRouter = require('./routes/contact')
 app.use('/contact', contactRouter)
 
 // PORT 
-const port = process.env.PORT || 1234
+const port = process.env.PORT || expressPort
 app.listen(port, () => console.log(`Listening on port ${port} ....`));

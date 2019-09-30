692b98578ef9ee96d25b1e7f7a3eb35631ac9534
wijdan1995 <wijdan.w.k@outlook.sa>
2019-09-30 13:41:50 +0300

    add alert

--- send-email.js
+++ send-email.js
@@ -1,21 +1,23 @@
 const sgMail = require('@sendgrid/mail');
 
-sgMail.setApiKey('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');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 function newEmail({ email, name, message }) {
-
+    const texstMsg = `The contact message from ${name} email: ${email} with this message : ${message}`
     const msg = {
         to: 'jooode2000@hotmail.com',
         from: email,
         subject: `Email from ${name}`,
-        text: message,
-        html: `<strong>${message}</strong>`,
+        text: texstMsg,
+        html: `<strong>${texstMsg}</strong>`,
     };
     sgMail.send(msg)
         .then(() => {
             //Celebrate
             console.log('SendGrid Done');
+            alert("Thank you for your message /n We'll contact you soon")
         })
+        .then(history.push('/'))
         .catch(error => {
 
             //Log friendly error
